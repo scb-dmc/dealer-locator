3d769d3f2ba9670633decad3acc1c207136143f3
Andy Summers <andrew.summers@santacruzbicycles.com>
2020-07-07 14:54:41 -0600

    Fix bad import

--- src/dealer-list.js
+++ src/dealer-list.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 import _ from "lodash";
 
-import reserve from "./theme";
+import { reserve } from "./theme";
 
 const dealerDistanceCalculator = (location) => {
   return (dealer) => {
